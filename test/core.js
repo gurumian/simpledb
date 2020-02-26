017aee3f7771796c88f29cb6a7ef4bfba286c357
Bryce Ghim <buttonfly@gmail.com>
2020-02-26 15:27:23 +0900

    update test

--- test/core.js
+++ test/core.js
@@ -1,9 +1,9 @@
-const {_Connection} = require('..');
+const {_Connection} = require('..')
 
-var chai = require('chai');
-var assert = chai.assert;    // Using Assert style
-var expect = chai.expect;    // Using Expect style
-var should = chai.should();  // Using Should style
+var chai = require('chai')
+var assert = chai.assert   // Using Assert style
+var expect = chai.expect    // Using Expect style
+var should = chai.should()  // Using Should style
 
 const fs = require('fs')
 
@@ -11,59 +11,57 @@ const db = 'example.db'
 const table = 'admin'
 
 describe('Core', function() {
-
   if (fs.existsSync(db)) {
-    fs.unlinkSync(db);
+    fs.unlinkSync(db)
   }
 
-  let connection = new _Connection(db);
+  let conn = new _Connection(db)
 
-  describe('Connection', function() {
+  describe('Create', function() {
     it('should return false when create() is failed', function() {
-      let stmt = connection.createStatement();
-      let query = `CREATE TABLE ${table}(idx INTEGER PRIMARY KEY AUTOINCREMENT, passwd TEXT, dump BLOB, date DATETIME);`;
+      let stmt = conn.createStatement()
+      let query = `CREATE TABLE ${table}(idx INTEGER PRIMARY KEY AUTOINCREMENT, passwd TEXT, dump BLOB, date DATETIME);`
       stmt.execute(query)
       .then(res => {
-        assert.equal(res, true);
-        assert.equal(fs.existsSync(db), true);
+        assert.equal(res, true)
       })
       .catch(err => {
-        console.log(err);
-      });
-    });
+        console.log(err)
+      })
+    })
 
     it('should return false when create() is failed', function() {
-      let stmt = connection.createStatement();
-      let query = `PRAGMA table_info(${table});`;
+      let stmt = conn.createStatement()
+      let query = `PRAGMA table_info(${table});`
       stmt.executeQuery(query)
       .then(res => {
-        assert.equal(res.next(), true);
-        let data = res.data;
-        assert.equal(data[0], 0);
-        assert.equal(data[1], 'idx');
-        assert.equal(data[2], 'INTEGER');
-        assert.equal(data[3], 0);
+        assert.equal(res.next(), true)
+        let data = res.data
+        assert.equal(data[0], 0)
+        assert.equal(data[1], 'idx')
+        assert.equal(data[2], 'INTEGER')
+        assert.equal(data[3], 0)
         
-        assert.equal(res.next(), true);
+        assert.equal(res.next(), true)
         data = res.data;
-        assert.equal(data[0], 1);
-        assert.equal(data[1], 'passwd');
-        assert.equal(data[2], 'TEXT');
-        assert.equal(data[3], 0);
+        assert.equal(data[0], 1)
+        assert.equal(data[1], 'passwd')
+        assert.equal(data[2], 'TEXT')
+        assert.equal(data[3], 0)
         
-        assert.equal(res.next(), true);
-        data = res.data;
-        assert.equal(data[0], 2);
-        assert.equal(data[1], 'dump');
-        assert.equal(data[2], 'BLOB');
-        assert.equal(data[3], 0);
+        assert.equal(res.next(), true)
+        data = res.data
+        assert.equal(data[0], 2)
+        assert.equal(data[1], 'dump')
+        assert.equal(data[2], 'BLOB')
+        assert.equal(data[3], 0)
 
-        assert.equal(res.next(), true);
-        data = res.data;
-        assert.equal(data[0], 3);
-        assert.equal(data[1], 'date');
-        assert.equal(data[2], 'DATETIME');
-        assert.equal(data[3], 0);
+        assert.equal(res.next(), true)
+        data = res.data
+        assert.equal(data[0], 3)
+        assert.equal(data[1], 'date')
+        assert.equal(data[2], 'DATETIME')
+        assert.equal(data[3], 0)
 
         // [ 0, 'idx', 'INTEGER', 0, null, 1 ]
         // [ 1, 'passwd', 'TEXT', 0, null, 0 ]
@@ -71,57 +69,59 @@ describe('Core', function() {
         // [ 3, 'date', 'DATETIME', 0, null, 0 ]
       })
       .catch(err => {
-        console.log(err);
-      });
-    });
+        console.log(err)
+      })
+    })
 
     it('should return false when create() is failed', function() {
-      let stmt = connection.createStatement();
-      let query = `PRAGMA table_info(${table});`;
+      let stmt = conn.createStatement()
+      let query = `PRAGMA table_info(${table});`
       stmt.executeQuery(query)
       .then(res => {
-        assert.equal(res.next(), true);
-        let obj = res.obj;
-        assert.equal(obj.hasOwnProperty('name'), true);
-        assert.equal(obj.name, 'idx');
-        assert.equal(obj.type, 'INTEGER');
+        assert.equal(res.next(), true)
+        let obj = res.obj
+        assert.equal(obj.hasOwnProperty('name'), true)
+        assert.equal(obj.name, 'idx')
+        assert.equal(obj.type, 'INTEGER')
 
-        assert.equal(res.next(), true);
-        obj = res.obj;
-        assert.equal(obj.hasOwnProperty('name'), true);
-        assert.equal(obj.name, 'passwd');
-        assert.equal(obj.type, 'TEXT');
+        assert.equal(res.next(), true)
+        obj = res.obj
+        assert.equal(obj.hasOwnProperty('name'), true)
+        assert.equal(obj.name, 'passwd')
+        assert.equal(obj.type, 'TEXT')
 
-        assert.equal(res.next(), true);
-        obj = res.obj;
-        assert.equal(obj.hasOwnProperty('name'), true);
-        assert.equal(obj.name, 'dump');
-        assert.equal(obj.type, 'BLOB');
+        assert.equal(res.next(), true)
+        obj = res.obj
+        assert.equal(obj.hasOwnProperty('name'), true)
+        assert.equal(obj.name, 'dump')
+        assert.equal(obj.type, 'BLOB')
 
-        assert.equal(res.next(), true);
-        obj = res.obj;
-        assert.equal(obj.hasOwnProperty('name'), true);
-        assert.equal(obj.name, 'date');
-        assert.equal(obj.type, 'DATETIME');
+        assert.equal(res.next(), true)
+        obj = res.obj
+        assert.equal(obj.hasOwnProperty('name'), true)
+        assert.equal(obj.name, 'date')
+        assert.equal(obj.type, 'DATETIME')
       })
       .catch(err => {
-        console.log(err);
+        console.log(err)
       });
     });
+  });
 
+  describe('Insert', function() {
     it('should return false when insert() is failed', function() {
-      let stmt = connection.prepareStatement(`INSERT INTO ${table} (passwd, dump, date) VALUES(?,?,datetime(\'now\',\'localtime\'));`);
+      let stmt = conn.prepareStatement(`INSERT INTO ${table} (passwd, dump, date) VALUES(?,?,datetime(\'now\',\'localtime\'));`)
       stmt.setString({
         index: 1,
         value: 'admin_passwd',
-      });
+      })
 
-      const obj = {hello: 'world'};
-      let str = JSON.stringify(obj, null, 2);
-      let arraybuffer = new ArrayBuffer(str.length * 2);
-      var bufView = new Uint16Array(arraybuffer);
+      const obj = {hello: 'world'}
+      let str = JSON.stringify(obj, null, 2)
+      let arraybuffer = new ArrayBuffer(str.length * 2)
+      var bufView = new Uint16Array(arraybuffer)
       for (var i=0, strLen=str.length; i < strLen; i++) {
-        bufView[i] = str.charCodeAt(i);
+        bufView[i] = str.charCodeAt(i)
       }
 
       stmt.setBlob({
@@ -131,22 +131,24 @@ describe('Core', function() {
 
       stmt.execute(false) // TODO: [Issue] if it's set true, it fails.
       .then(res => {
-        assert.equal(res, true);
+        assert.equal(res, true)
       })
       .catch(err => {
-        console.log(err);
+        console.log(err)
       });
     });
+  });
 
+  describe('Select', function() {
     it('should return error when select() is failed', function() {
-      let stmt = connection.createStatement();
-      let query = `SELECT idx, passwd, dump, date FROM ${table}`;
+      let stmt = conn.createStatement()
+      let query = `SELECT idx, passwd, dump, date FROM ${table}`
       stmt.executeQuery(query)
       .then(res => {
-        assert.equal(res.next(), true);
-        let data = res.data;
-        assert.equal(data[0], 1);
-        assert.equal(data[1], 'admin_passwd');
+        assert.equal(res.next(), true)
+        let data = res.data
+        assert.equal(data[0], 1)
+        assert.equal(data[1], 'admin_passwd')
         // [
         //   1,
         //   'admin_passwd',
@@ -158,12 +160,14 @@ describe('Core', function() {
         // ]
       })
       .catch(err => {
-        console.log(err);
+        console.log(err)
       });
     });
+  });
 
+  describe('Update', function() {
     it('should return false when update() is failed', function() {
-      let stmt = connection.prepareStatement(`UPDATE ${table} set passwd=?, date=datetime(\'now\',\'localtime\') WHERE idx=1;`);
+      let stmt = conn.prepareStatement(`UPDATE ${table} set passwd=?, date=datetime(\'now\',\'localtime\') WHERE idx=1;`)
       stmt.setString({
         index: 1,
         value: 'new_passcode',
@@ -178,51 +182,53 @@ describe('Core', function() {
     });
 
     it('should return error when select() is failed', function() {
-      let stmt = connection.createStatement();
-      let query = `SELECT idx, passwd, date FROM ${table}`;
+      let stmt = conn.createStatement();
+      let query = `SELECT idx, passwd, date FROM ${table}`
       stmt.executeQuery(query)
       .then(res => {
-        assert.equal(res.next(), true);
+        assert.equal(res.next(), true)
         let data = res.data;
-        assert.equal(data[0], 1);
-        assert.equal(data[1], 'new_passcode');
+        assert.equal(data[0], 1)
+        assert.equal(data[1], 'new_passcode')
       })
       .catch(err => {
-        console.log(err);
-      });
-    });
+        console.log(err)
+      })
+    })
+  })
 
+  describe('Delete', function() {
     it('should return false when delete() is failed', function() {
-      let stmt = connection.prepareStatement(`DELETE FROM ${table} WHERE idx=?;`);
-      let id = 2;
+      let stmt = conn.prepareStatement(`DELETE FROM ${table} WHERE idx=?;`)
+      let id = 2
       stmt.setInt({
         index: 1,
         value: id,
       });
       stmt.execute()
       .then(res => {
-        assert.equal(res, true);
+        assert.equal(res, true)
       })
       .catch(err => {
-        console.log(err);
+        console.log(err)
       });
     });
 
     it('should return false when delete() is failed', function() {
-      let stmt = connection.createStatement();
-      let id = 1;
-      let query = `DELETE FROM ${table} WHERE idx=${id};`;
+      let stmt = conn.createStatement()
+      let id = 1
+      let query = `DELETE FROM ${table} WHERE idx=${id};`
       stmt.execute(query)
       .then(res => {
-        assert.equal(res, true);
+        assert.equal(res, true)
       })
-    });
-  });
+    })
+  })
 
   describe('Performance', function() {
     it('should return false when insert() is failed', function() {
       for(var i = 0; i < 1000; i++) {
-        let stmt = connection.prepareStatement(`INSERT INTO ${table} (passwd, date) VALUES(?,datetime(\'now\',\'localtime\'));`);
+        let stmt = conn.prepareStatement(`INSERT INTO ${table} (passwd, date) VALUES(?,datetime(\'now\',\'localtime\'));`)
         stmt.setString({
           index: 1,
           value: 'admin_passwd',
@@ -230,51 +236,52 @@ describe('Core', function() {
 
         stmt.execute()
         .then(res => {
-          assert.equal(res, true);
+          assert.equal(res, true)
         })
         .catch(err => {
-          console.log(err);
-        });
+          console.log(err)
+        })
       }
-    });
+    })
 
     it('should return false when delete() is failed', function() {
-      let stmt = connection.createStatement();
-      let query = `DELETE FROM ${table};`;
+      let stmt = conn.createStatement()
+      let query = `DELETE FROM ${table};`
       stmt.execute(query)
       .then(res => {
-        assert.equal(res, true);
+        assert.equal(res, true)
       })
       .catch(err => {
-        console.log(err);
+        console.log(err)
       });
     });
 
+    let count = 1000
     it('should return false when insert() is failed', function() {
-      for(var i = 0; i < 1000; i++) {
-        let stmt = connection.createStatement();
-        let passwd = `admin_passwd${i}`;
-        let query = `INSERT INTO ${table} (passwd, date) VALUES(\'${passwd}\',datetime(\'now\',\'localtime\'));`;
+      for(var i = 0; i < count; i++) {
+        let stmt = conn.createStatement()
+        let passwd = `admin_passwd${i}`
+        let query = `INSERT INTO ${table} (passwd, date) VALUES(\'${passwd}\',datetime(\'now\',\'localtime\'));`
         stmt.execute(query)
         .then(res => {
-          assert.equal(res, true);
+          assert.equal(res, true)
         })
         .catch(err => {
-          console.log(err);
+          console.log(err)
         });
       }
     });
     
     it('should return false when select count(*) is failed', function() {
-      let stmt = connection.createStatement();
-      let query = `SELECT COUNT(*) FROM ${table};`;
+      let stmt = conn.createStatement()
+      let query = `SELECT COUNT(*) FROM ${table};`
       stmt.executeQuery(query)
       .then(res => {
-        res.next();
-        assert.equal(res.data[0], 1000);
+        assert.equal(res.next(), true)
+        assert.equal(res.data[0], count)
       })
       .catch(err => {
-        console.log(err);
+        console.log(err)
       });
     });
   });
