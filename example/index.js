1c4dda77e9c1f36c725523ce60ec210b3f6f95f9
Bryce Ghim <buttonfly@gmail.com>
2020-02-27 12:54:42 +0900

    update example

--- example/index.js
+++ example/index.js
@@ -11,41 +11,41 @@ if(args.length) {
   db = args[0];
 }
 
-async function create() {
+function create() {
   let conn = new Connection(db)
   let stmt = conn.createStatement()
   let query = `CREATE TABLE ${table}(idx INTEGER PRIMARY KEY AUTOINCREMENT, passwd TEXT, date DATETIME);`
-  return await stmt.execute({
+  return stmt.execute({
     query: query,
     async: true,
   })
 }
 
-async function select() {
+function select() {
   let conn = new Connection(db)
   let stmt = conn.createStatement()
   let query = `SELECT idx, passwd, date FROM ${table}`;
-  return await stmt.execute({
+  return stmt.execute({
     query: query,
   })
 }
 
-async function update() {
+function update() {
   let stmt = conn.createStatement();
   let password = 'new password';
   let query = `UPDATE ${table} set passwd=\'${password}\', date=datetime(\'now\',\'localtime\') WHERE idx=1;`;
-  return await stmt.execute({
+  return stmt.execute({
     query: query,
     async: true,
   })
 }
 
-async function _delete() {
+function _delete() {
   let conn = new Connection(db)
   let stmt = conn.createStatement()
   let id = 1
   let query = `DELETE FROM ${table} WHERE idx=${id};`;
-  return await stmt.execute({
+  return stmt.execute({
     query: query,
   })
 }
